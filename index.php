7d13b711048d0eea0546d0e4b8bab4f9d7e404f1
eugenekuprikov <cualcua@gmail.com>
2019-02-03 22:25:57 +0200

    Заполняем список lots__list объявлениями, используя цикл для прохода по массиву с товарами

--- index.php
+++ index.php
@@ -112,18 +112,18 @@ $announce_list = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php foreach ($announce_list as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$item['URL картинки']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$item['Категория']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['Название']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$item['Цена']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -131,6 +131,7 @@ $announce_list = [
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
