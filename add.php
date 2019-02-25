09e50571ec94cd150272ac4701422c5ef973f9ce
eugenekuprikov <cualcua@gmail.com>
2019-02-25 19:59:46 +0200

    Создаем массив категорий и подключаем в шаблон

---
+++ add.php
@@ -0,0 +1,23 @@
+<?php
+require_once('init.php');
+require_once('functions.php');
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    show_error($page_content, $error);
+}
+else {
+    $sql = 'SELECT `id`, `name` FROM categories';
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        $error = mysqli_error($link);
+        show_error($page_content, $error);
+    }
+
+    $page_content = include_template('add.php', ['categories' => $categories]);
+
+?>
