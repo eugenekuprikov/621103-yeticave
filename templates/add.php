c44867e0758a40de8e7bb5b0a5428eefc9c91692
eugenekuprikov <cualcua@gmail.com>
2019-02-25 19:17:00 +0200

    Добавляем атрибут name полю с типом file

--- templates/add.php
+++ templates/add.php
@@ -46,7 +46,7 @@
       </div>
     </div>
     <div class="form__input-file">
-      <input class="visually-hidden" type="file" id="photo2" value="">
+      <input class="visually-hidden" type="file" name="announce_img" id="photo2" value="">
       <label for="photo2">
         <span>+ Добавить</span>
       </label>
