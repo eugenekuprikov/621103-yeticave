c1ca05288eceb4e25a40894839b9471feb3bde24
eugenekuprikov <cualcua@gmail.com>
2019-03-02 12:05:18 +0200

    Восстанавливаем файл со вспомогательными функциями

--- functions.php
+++ functions.php
@@ -1,18 +1,71 @@
 <?php
+function db_get_prepare_stmt($link, $sql, $data = []) {
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = null;
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_string($value)) {
+                $type = 's';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        $func = 'mysqli_stmt_bind_param';
+        $func(...$values);
+    }
+
+    return $stmt;
+}
+
+function show_error(&$content, $error) {
+    $content = include_template('error.php', ['error' => $error]);
+}
+
 function include_template($name, $data) {
     $name = 'templates/' . $name;
     $result = '';
 
-    if (!is_readable($name)) {
+    if (!file_exists($name)) {
         return $result;
     }
 
     ob_start();
     extract($data);
-    require $name;
+    require_once $name;
 
     $result = ob_get_clean();
 
+    return $result;
+}
+
+/**
+ * Проверяет, что переданная дата соответствует формату ДД.ММ.ГГГГ
+ * @param string $date строка с датой
+ * @return bool
+ */
+function check_date_format($date) {
+    $result = false;
+    $regexp = '/(\d{2})\.(\d{2})\.(\d{4})/m';
+    if (preg_match($regexp, $date, $parts) && count($parts) == 4) {
+        $result = checkdate($parts[2], $parts[1], $parts[3]);
+    }
     return $result;
 }
 ?>
